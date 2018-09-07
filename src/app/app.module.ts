54d63c0a8dcc7655c46450c5b193ac88706243de
tokash <yossi.tokash@gmail.com>
2018-09-07 09:49:41 +0300

    Added Stock data component & router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StockDataModelComponent } from './stock-data-model/stock-data-model.component';
@@ -7,17 +8,33 @@ import { StockDataServiceService } from './Services/stock-data-service.service';
 import { HttpClientModule } from '@angular/common/http';
 import { StocksNavbarComponent } from './Components/stocks-navbar/stocks-navbar.component';
 import { StocksTableComponent } from './Components/stocks-table/stocks-table.component';
+import { StockDataComponent } from './Components/stock-data/stock-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StockDataModelComponent,
     StocksNavbarComponent,
-    StocksTableComponent
+    StocksTableComponent,
+    StockDataComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: StocksTableComponent
+        },
+        {
+          path: 'stock-data/:stock_name',
+          component: StockDataComponent
+        }
+
+      ]
+    )
+
   ],
   providers: [StockDataServiceService],
   bootstrap: [AppComponent]
