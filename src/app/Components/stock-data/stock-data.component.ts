bdcabbd0035720cf765e56fda5d56d2c474083aa
tokash <yossi.tokash@gmail.com>
2018-09-09 22:47:59 +0300

    Added chart.js graph & getting data from the server for a company

--- src/app/Components/stock-data/stock-data.component.ts
+++ src/app/Components/stock-data/stock-data.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import { WeatherService } from '../../Services/weather.service';
+import {Chart} from 'chart.js'
+import { StockDataServiceService } from '../../Services/stock-data-service.service';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-stock-data',
@@ -6,10 +11,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-data.component.css']
 })
 export class StockDataComponent implements OnInit {
+  chart = [];
+  paramStockName = '';
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _weather: WeatherService,
+              private _stockData: StockDataServiceService) { }
 
+  // OnInit,
   ngOnInit() {
+    
+    this._route.queryParams.subscribe(params => {this.paramStockName = params['stock_name']}) //['stock_name']
+    //console.log(this.paramStockName)
+     this._stockData.getStockData(this.paramStockName)
+      .subscribe(res => {
+        console.log(res)
+     })
+
+    this._weather.dailyForecast()
+    .subscribe(res => {
+      //console.log(res)
+
+      let temp = res['list'].map(res => res.main.temp)
+      // let temp_max = res['list'].map(res => res.main.temp_max)
+      // let temp_min = res['list'].map(res => res.main.temp_min)
+      let pressure = res['list'].map(res => res.main.pressure)
+      let allDates = res['list'].map(res => res.dt)
+
+      // console.log(temp)
+      // console.log(temp_max)
+      // console.log(temp_min)
+      // console.log(pressure)
+
+      let weatherDates = []
+      allDates.forEach((res) => {
+        let jsdate = new Date(res * 1000)
+        weatherDates.push(jsdate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}))
+      });
+
+      //console.log(weatherDates);
+      let chart = new Chart('canvas', {
+        type: 'line',
+        data  : {
+          labels: weatherDates,
+          datasets: [
+            {
+              label: "Average Temp.",
+              data: temp,
+              borderColor: '#3cbb9f',
+              fill: 'false'
+            },
+            {
+              label: "Pressure",
+              data: pressure,
+              borderColor: '#ccff00',
+              fill: 'false'
+            },
+          ],
+          options:{
+            responsive: true,
+            // legend: {
+            //   display: false,
+            // },
+            title: {
+              display: true,
+              position: "top",
+              text: "Moscow Temp. & Pressure",
+            },
+            // tooltips: {
+            //   mode: 'index',
+            //   intersect: true,
+            // },
+            // hover: {
+            //   mode: 'nearest',
+            //   intersect: true
+            // },
+            // scales: {
+            //   xAxes: [{
+            //     display: true,
+            //     scaleLabel: {
+            //       display: true,
+            //       labelString: 'Month'
+            //     }
+            //   }],
+            //   yAxes: [{
+            //     display: true,
+            //     scaleLabel: {
+            //       display: true,
+            //       labelString: 'Value'
+            //     }
+            //   }]
+            // }
+          }
+        }
+      });
+
+    })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { StockDataModelComponent } from './stock-data-model/stock-data-model.component';
 import { StockDataServiceService } from './Services/stock-data-service.service';
-import { HttpClientModule } from '@angular/common/http';
 import { StocksNavbarComponent } from './Components/stocks-navbar/stocks-navbar.component';
 import { StocksTableComponent } from './Components/stocks-table/stocks-table.component';
 import { StockDataComponent } from './Components/stock-data/stock-data.component';
+import { WeatherService } from './Services/weather.service';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent,
     StockDataModelComponent,
     StocksNavbarComponent,
     StocksTableComponent,
-    StockDataComponent
+    StockDataComponent,   
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot(
       [
         {
@@ -28,15 +33,18 @@ import { StockDataComponent } from './Components/stock-data/stock-data.component
           component: StocksTableComponent
         },
         {
-          path: 'stock-data/:stock_name',
+          path: 'stock-data', //:stock_name
           component: StockDataComponent
+        },
+        {
+          path: '**',
+          component: StocksTableComponent
         }
-
       ]
     )
 
   ],
-  providers: [StockDataServiceService],
+  providers: [StockDataServiceService, WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
